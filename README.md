26fe143ff785e359d2605df310ca28cf7f8dcd09
Kerolos Lotfy <100154566+KerolosLotfy@users.noreply.github.com>
2025-09-08 06:46:58 +0300

    Add Font Awesome icons, fix button functionality, and add Enter key support

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
 # Calculator Application
 
-### descripton:-
-Sample Calculator App 
+## Description:-
 
-### Versions:-
- - #### v1
+Sample Calculator App
+
+## Versions:-
+
+- ### v1
+
     Sample
- - #### v2
+
+- ### v2
+
     Improve Code and add themes and add more operations such as power
-    
- - #### v3
+
+- ### v3
+
     change Design
-    
-### Links:- 
-   - **[Calculator Version 1](https://keroloslotfy.github.io/calculator/)**
-   - **[Calculatore Version 2](https://keroloslotfy.github.io/calculator/v2)**
-   - **[Calculator Version 3](https://keroloslotfy.github.io/calculator/v3)**
 
-### Used Lang:-
- - HTML
- - CSS
- - JavaScript
+## Links:-
 
-### Author:-
+- **[Calculator](https://keroloslotfy.github.io/calculator/)**
+
+## Used Lang:-
+
+- HTML
+- CSS
+- JavaScript
+
+## Author:-
+
 Kerolos Lotfy
--- js/app.js
+++ js/app.js
@@ -1,41 +1,55 @@
 const screen = document.querySelector(".screen");
 const buttons = document.querySelectorAll(".box > div > div");
+const equalButton = document.getElementById("equal");
 
 buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        playAudio();
-        if (btn.id === "del") {
-            let arr = screen.value.split("");
-            arr.pop();
-            screen.value = arr.join("");
-        }
-        else if (btn.id === "clear") {
-            screen.value = "";
-        }
-        else if (btn.id === "equal") {
-            screen.value = eval(screen.value)
-        }
-        else if (btn.id === "sqrt") {
-            screen.value = Math.sqrt(eval(screen.value))
-        }
-        else if (btn.id === "half") {
-            if (typeof eval(screen.value) === "number") {
-                screen.value = eval(screen.value) / 2;
-            } else {
-                screen.value = "Error";
-                screen.style.color
-            }
-        }
-        else {
-
-            screen.value += btn.innerText;
-        }
-    });
+  btn.addEventListener("click", () => {
+    playAudio();
+    if (btn.id === "del") {
+      let arr = screen.value.split("");
+      arr.pop();
+      screen.value = arr.join("");
+    } else if (btn.id === "clear") {
+      screen.value = "";
+    } else if (btn.id === "equal") {
+      screen.value = eval(screen.value);
+    } else if (btn.id === "sqrt") {
+      screen.value = Math.sqrt(eval(screen.value));
+    } else if (btn.id === "half") {
+      if (typeof eval(screen.value) === "number") {
+        screen.value = eval(screen.value) / 2;
+      } else {
+        screen.value = "Error";
+        screen.style.color;
+      }
+    } else {
+      // Handle different button types
+      if (btn.id.startsWith("n")) {
+        // Number buttons (n1, n2, etc.)
+        screen.value += btn.id.charAt(1);
+      } else if (btn.id === "dot") {
+        screen.value += ".";
+      } else if (btn.id === "sum") {
+        screen.value += "+";
+      } else if (btn.id === "d") {
+        screen.value += "-";
+      } else if (btn.id === "multi") {
+        screen.value += "*";
+      } else if (btn.id === "dev") {
+        screen.value += "/";
+      }
+    }
+  });
 });
-
-
 
 //  function to play audio after button clicked
 function playAudio() {
-    document.querySelector("audio").play();
+  document.querySelector("audio").play();
 }
+
+// trigger equal on Enter key
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && equalButton) {
+    equalButton.click();
+  }
+});
